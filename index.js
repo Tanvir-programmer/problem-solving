681a5a5c28c5ab363f2af75bbd5678a00a2360af
Tanvir-programmer <tanvir.webprogrammer@gmail.com>
2026-03-25 10:53:26 +0600

    updated

--- index.js
+++ index.js
@@ -1,10 +1,36 @@
 //reverse string
-function reverse(str) {
-  let reverse = "";
-  for (char of str) {
-    reverse = char + reverse;
-  }
-  return reverse;
-}
+// function reverse(str) {
+//   let reverse = "";
+//   for (char of str) {
+//     reverse = char + reverse;
+//   }
+//   return reverse;
+// }
 
-console.log(reverse("yallah ya"));
+// console.log(reverse("yallah ya"));
+
+// -- using building functions --
+
+// function reverse(str){
+//     return str.split("").reverse().join("")
+// }
+// console.log(reverse("yallah ya"));
+
+// function reverseInt(n) {
+//   return (reverse = n.toString().split("").reverse().join(""));
+// }
+// console.log(reverseInt(51));
+
+// find the max char
+
+function maxChar(str){
+    const charMap = {}
+    for(let char of str){
+        if(charMap[char]){
+            charMap[char ]=charMap[char]+1
+        }else{
+            charMap[char] = 1
+            
+        }
+    }
+}
